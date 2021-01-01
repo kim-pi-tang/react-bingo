e6aa0a832905f6196b822b1429b20fb529fa077d
mskwon1 <mageeeeek@gmail.com>
2021-01-01 17:11:22 +0900

    CHORE: add material-ui & styled-components & react-router-dom
    delete unnecessary files & contents

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, Typography } from '@material-ui/core';
+import styled from 'styled-components';
+
+const MainContainer = styled(Container)`
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainContainer maxWidth="xl">
+      <Typography variant="h3">반갑다 세상</Typography>
+      <Typography variant="body1" paragraph>
+        시작한다 개발
+      </Typography>
+    </MainContainer>
   );
 }
 
