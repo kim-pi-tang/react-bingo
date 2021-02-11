76ee06524e5aa25a3e7f0dea36b1fc6bc059152e
JounJongSuck <mone123123@naver.com>
2021-02-12 00:10:29 +0900

    로그인 페이지 그리드로 변경

--- web/src/pages/LoginPage.js
+++ web/src/pages/LoginPage.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import { getWindowDimensions } from '../utils/PageUtil';
 
 // css
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
       margin: theme.spacing(5),
-      width: '50ch',
+      width: getWindowDimensions().width > 410 ? '40ch' : getWindowDimensions().width - 50,
     },
   },
 }));
@@ -18,26 +20,26 @@ function LoginPage() {
   const classes = useStyles();
 
   return (
-    <div style={{ textAlign: 'center' }}>
-      <form className={classes.root} noValidate autoComplete="off">
-        <img
-          src="https://mblogthumb-phinf.pstatic.net/20110323_149/yean5rang_1300858060958TRSBY_JPEG/%B1%D7%B7%B1%B0%C7_%BE%F8%B4%D9.jpg?type=w2"
-          alt="gonawoo"
-        />
-        <br />
-        <TextField id="standard-full-width" label="아이디" fullWidth />
-        <br />
-        <TextField id="standard-full-width" label="비밀번호" fullWidth />
-        <br />
-        <Button size="small" variant="contained" color="primary" fullWidth onClick={abc}>
-          로그인 하기
-        </Button>
-        <br />
-        <Button size="small" variant="contained" color="secondary" fullWidth>
-          계정 새로 만들기
-        </Button>
-      </form>
-    </div>
+    <Grid
+      container
+      direction="column"
+      justify="center"
+      alignItems="center"
+      className={classes.root}
+    >
+      <img
+        src="https://mblogthumb-phinf.pstatic.net/20110323_149/yean5rang_1300858060958TRSBY_JPEG/%B1%D7%B7%B1%B0%C7_%BE%F8%B4%D9.jpg?type=w2"
+        alt="gonawoo"
+      />
+      <TextField id="standard-full-width" label="아이디" />
+      <TextField id="standard-full-width" label="비밀번호" />
+      <Button size="small" variant="contained" color="primary" onClick={abc}>
+        로그인 하기
+      </Button>
+      <Button size="small" variant="contained" color="secondary">
+        계정 새로 만들기
+      </Button>
+    </Grid>
   );
 }
 
--- web/src/utils/PageUtil.js
+++ web/src/utils/PageUtil.js
@@ -5,3 +5,11 @@ export async function getDataFromUrl({ url }) {
   const response = await axios.get(`${serverBaseUrl}/${url}`);
   return response.data;
 }
+
+export function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
