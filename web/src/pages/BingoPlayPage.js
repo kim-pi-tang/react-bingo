1564c5b774f4b5fd38c076a481fe2d75117c9c64
LeKAKiD <lekakid@kakao.com>
2021-02-19 19:58:41 +0900

    REFACTOR: move result buttons to play page
    Need to talk about make statistics page

--- web/src/pages/BingoPlayPage.js
+++ web/src/pages/BingoPlayPage.js
@@ -1,9 +1,19 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
-import { Button, Collapse, Container, Grid, Paper, Typography } from '@material-ui/core';
-import DoneIcon from '@material-ui/icons/Done';
+import {
+  Button,
+  CircularProgress,
+  Collapse,
+  Grid,
+  IconButton,
+  Paper,
+  Snackbar,
+  Typography,
+} from '@material-ui/core';
 import { styled } from '@material-ui/core/styles';
-import { useBingoState, useBingoDispatch, getBingo, refreshBingo } from '../contexts/BingoContext';
+import { Create, Done, Link, RotateLeft, Share } from '@material-ui/icons';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { useBingoState, useBingoDispatch, getBingo } from '../contexts/BingoContext';
 import BingoBoard from '../components/BingoBoard';
 
 const CountContainer = styled(Paper)({
@@ -12,67 +22,137 @@ const CountContainer = styled(Paper)({
   paddingBottom: '0.5rem',
 });
 
+function SaveButton({ onSave }) {
+  return (
+    <>
+      <Grid item sm={3} />
+      <Grid item xs={12} sm={6}>
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          startIcon={<Done />}
+          fullWidth
+          style={{ marginTop: '0.5rem' }}
+          onClick={onSave}
+        >
+          결과보기
+        </Button>
+      </Grid>
+      <Grid item sm={3} />
+    </>
+  );
+}
+
+function ResultButton({ shareLink }) {
+  const history = useHistory();
+  const [isSnackOpen, setSnackOpen] = useState(false);
+
+  const onClickShareLink = () => {
+    // 클립보드에 복사
+    setSnackOpen(true);
+  };
+  const onCloseShareLink = () => {
+    setSnackOpen(false);
+  };
+
+  return (
+    <>
+      <Grid item xs={12} sm={6}>
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          startIcon={<RotateLeft />}
+          fullWidth
+          onClick={() => history.push('/')}
+        >
+          빙고 목록으로
+        </Button>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Button variant="contained" color="primary" size="large" startIcon={<Create />} fullWidth>
+          빙고 제작
+        </Button>
+      </Grid>
+      <Grid item xs={12}>
+        <CopyToClipboard text={shareLink}>
+          <IconButton onClick={onClickShareLink}>
+            <Link />
+          </IconButton>
+        </CopyToClipboard>
+        <IconButton>
+          <Share />
+        </IconButton>
+        <IconButton>
+          <Share />
+        </IconButton>
+        <IconButton>
+          <Share />
+        </IconButton>
+        <IconButton>
+          <Share />
+        </IconButton>
+        <IconButton>
+          <Share />
+        </IconButton>
+        <Snackbar
+          open={isSnackOpen}
+          autoHideDuration={3000}
+          onClose={onCloseShareLink}
+          message="빙고 주소가 복사되었습니다."
+        />
+      </Grid>
+    </>
+  );
+}
+
 function BingoPlayPage() {
   const { id } = useParams();
-  const history = useHistory();
-
   const state = useBingoState();
   const dispatch = useBingoDispatch();
+  const [isResult, setResult] = useState(false);
 
   useEffect(() => {
     getBingo(dispatch, id);
   }, [dispatch, id]);
 
-  const { loading, data, error } = state.bingo;
-  const { progress } = state;
-
   // 결과보기 버튼 클릭시 호출할 함수.
   const onSave = useCallback(async () => {
     dispatch({ type: 'SUBMIT_BINGO_RESULT' });
-    await refreshBingo(dispatch, id);
-    history.push(`/bingo/${id}/result`);
-  }, [dispatch, id, history]);
+    setResult(true);
+  }, [dispatch]);
 
-  if (data) {
-    return (
-      <Grid container spacing={2} align="center">
-        <Grid item xs={12}>
-          <BingoBoard data={data} progress={progress} playable />
-        </Grid>
-        <Grid item sm={3} />
-        <Grid item xs={12} sm={6}>
-          <Collapse in={progress.bingoCount > 0}>
-            <CountContainer elevation={3}>
-              <Typography>{progress.bingoCount} 빙고!</Typography>
-            </CountContainer>
-          </Collapse>
-        </Grid>
-        <Grid item sm={3} />
-        <Grid item sm={3} />
-        <Grid item xs={12} sm={6}>
-          <Button
-            variant="contained"
-            color="primary"
-            size="large"
-            startIcon={<DoneIcon />}
-            fullWidth
-            style={{ marginTop: '0.5rem' }}
-            onClick={onSave}
-          >
-            결과보기
-          </Button>
-        </Grid>
-        <Grid item sm={3} />
-      </Grid>
-    );
-  } else {
-    return (
-      <Container maxWidth="sm">
-        {loading && <div>Loading</div>}
-        {error && <div>Error: {error.message}</div>}
-      </Container>
-    );
+  const { loading, data, error } = state.bingo;
+  const { progress } = state;
+  const shareURL = `${window.location.origin}/bingo/${id}`;
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
   }
+
+  if (loading) {
+    return <CircularProgress />;
+  }
+
+  return (
+    <Grid container spacing={2} align="center">
+      <Grid item xs={12}>
+        {data && <BingoBoard data={data} progress={progress} playable={!isResult} />}
+      </Grid>
+      <Grid item sm={3} />
+      <Grid item xs={12} sm={6}>
+        <Collapse in={progress.bingoCount > 0}>
+          <CountContainer elevation={3}>
+            <Typography>{progress.bingoCount} 빙고!</Typography>
+          </CountContainer>
+        </Collapse>
+      </Grid>
+      <Grid item sm={3} />
+      {!isResult && <SaveButton onSave={onSave} />}
+      {isResult && <ResultButton shareLink={shareURL} />}
+    </Grid>
+  );
 }
 
 export default BingoPlayPage;
